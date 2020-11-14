8eaf546d54db5485458016416f297bb116935268
Omri <omriyonatani@gmail.com>
2020-11-14 17:44:00 +0200

    Initial commit

---
+++ Assets/Scripts/3-collisions/DestroyOnTriggerWithLives.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+/**
+ * This component destroys its object whenever it triggers a 2D collider with the given tag, with a SerializedField lives
+ */
+public class DestroyOnTriggerWithLives : MonoBehaviour{
+
+    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+    [SerializeField] string triggeringTag="Enemy";
+    [Tooltip("Number of lives")]
+    [SerializeField] int lives = 3;
+    [SerializeField] TextMeshPro textMesh;
+    string prefix = "Lives: ";
+
+    void Start(){
+        if (textMesh.text.Length != 0){
+            prefix = textMesh.text;
+        }
+        textMesh.text = prefix + lives.ToString();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == triggeringTag && enabled){
+            lives--;
+            textMesh.text = prefix + lives.ToString();
+            if (lives == 0){
+                Destroy(this.gameObject);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/3-collisions/InvisibleWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleWall : MonoBehaviour{
+    [Tooltip("Every object over the wall will destroy")]
+    [SerializeField] string triggeringTag;
+
+    // Invisible wall- every other (Enemy/Laser) tag that over - will destroy.
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == triggeringTag){
+            Destroy(other.gameObject);
+        }
+    }
+}
